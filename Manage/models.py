365e17c528f4c3a587dc3e80d36f01de09c81266
anishsatalkar <anishsatalkar@gmail.com>
2018-05-13 22:05:05 +0530

    Added country_code to Visitor model.

--- Manage/models.py
+++ Manage/models.py
@@ -19,6 +19,8 @@ class Visitor(models.Model):
     purpose_of_visit = models.CharField(max_length=500, null=True, blank=True)
     date_time_of_entry = models.DateTimeField(default=timezone.now)
 
+    country_code = models.CharField(max_length=10, null=True, blank=True)
+
     def __str__(self):
         return self.first_name + " " + self.last_name
 
@@ -27,13 +29,12 @@ class Address(models.Model):
     visitor = models.ForeignKey(Visitor, on_delete=models.CASCADE)
     building = models.CharField(max_length=50, blank=True, null=True)
     pin_code = models.PositiveIntegerField()
-    street = models.CharField(max_length=150,blank=True , null=True)
+    street = models.CharField(max_length=150, blank=True, null=True)
     city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50 , blank=True , null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=100)
 
     def __str__(self):
-
-
-        return str(self.building) + " " + str(self.street) + " " + str(self.city) + " " + str(self.state) + " " + str(self.country) + " " + str(
+        return str(self.building) + " " + str(self.street) + " " + str(self.city) + " " + str(self.state) + " " + str(
+            self.country) + " " + str(
             self.pin_code)
